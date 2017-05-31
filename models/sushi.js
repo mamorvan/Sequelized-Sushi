d409d4a9f75d01a793dfdeee92d53c82537c10dd
Mary Morvan <mamorvan@yahoo.com>
2017-05-31 10:01:36 -0400

    add notEmpty to validation :sushi:

--- models/sushi.js
+++ models/sushi.js
@@ -2,7 +2,8 @@ module.exports = function(sequelize, DataTypes){
 	var Sushi = sequelize.define("Sushi", {
 		sushi_name: {
 			type: DataTypes.STRING,
-			allowNull: false
+			allowNull: false,
+			notEmpty: true
 		},
 		devoured: {
 			type: DataTypes.BOOLEAN,
